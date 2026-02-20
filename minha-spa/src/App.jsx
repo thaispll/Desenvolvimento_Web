0dfca649583b08147ed1b6cd276650eda5f59eb3
Thais Leal <thais.pll@hotmail.com>
2026-02-19 21:30:18 -0300

    aula até dia 19.02

--- minha-spa/src/App.jsx
+++ minha-spa/src/App.jsx
@@ -2,7 +2,6 @@ import {BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import {Navbar} from './components/Navbar';
 import {Sobre} from './pages/Sobre'
 import {Home} from './pages/Home';
-
 //Rotas no React: mecanismo para navegação entre diferentes páginas ou componentes em uma aplicação  de página única(SPA)
 
 //importação direta de uma página
@@ -10,11 +9,11 @@ function App(){
   return (
     <BrowserRouter>
       <Navbar/>
-
       <main style={{padding: '2px'}}>
         <Routes>
           <Route path='/' element={<Home/>}></Route>
           <Route path='/sobre' element={<Sobre/>}></Route>
+
         </Routes>
       </main>
     </BrowserRouter>
--- minha-spa/src/pages/Sobre.jsx
+++ minha-spa/src/pages/Sobre.jsx
@@ -1,28 +1,27 @@
-import styles from '../styles/Sobre.module.css'
+import styles from '../styles/Sobre.module.css' ;
 
 export function Sobre(){
     return(
         <div className={styles.container}>
             <header className={styles.header}>
-                <h1 className={styles.title}>Sobre o projeto</h1>
-                <p className={styles.subtitle}>Conheça as tecnologias aplicadas nessa SPA</p>
+                <h1 className={styles.title}> Sobre o projeto</h1>
+                <p className={styles.subtitle}>Conheça as tecnologias aplicadas nessa SPA.</p>
             </header>
 
-            <section className={styles.content}>
-                <p>Este projeto está sendo construído como prática para domínio do <strong>React</strong></p>
-
-                <div className={styles.card}>
-                    <h3>Capacidades técnicas:</h3>
-                    <ul className={styles.list}>
-                        <li>Estrutura do Framework</li>
-                        <li>Gerenciamento de Rotas</li>
-                        <li>Estilização por componentes</li>
-                    </ul>
-                </div>
-            </section>
-            <footer className={styles.footer}>
-                <p>Desenvolvido durante o aprendizado de Front-end</p>
-            </footer>
+        <section className={styles.content}>
+        <p>Este projeto bla bla bla bla <strong> React</strong></p>
+        <div className={styles.card}>
+            <h3>Capacidades Técnicas:</h3>
+            <ul className={styles.list}>
+            <li>Estrutura de Frame</li>
+            <li>Gerenciamento de Rotas</li>
+            <li>Estilização por componentes</li>
+            </ul>
+        </div>
+        </section>
+        <footer className={styles.footer}>
+        <p>Desenvolvido durante o aprendizado de Front-End</p>
+        </footer>
         </div>
     );
 }
