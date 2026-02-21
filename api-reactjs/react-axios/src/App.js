ef9c798772b219189c16e75fe9c8637b9d0adce4
Thais Leal <thais.pll@hotmail.com>
2026-02-20 21:29:06 -0300

    aula até 20.02

---
+++ api-reactjs/react-axios/src/App.js
@@ -0,0 +1,30 @@
+import React, {useEffect, useState} from "react";
+import api from "./services/api";
+
+export default function App() {
+  return (
+    const[user, setUser] = useState(null);
+    
+    //carrega dados da API
+    useEffect(() =>{
+      async function loadUser(){
+        try{
+          const response = await api.get('/user');
+          setUser(response.data);
+        } catch(error){
+          console.error('Erro', error)
+        }   
+      }
+      loadUser();
+    }, []);
+
+    if(!user) return <div>Carregando...</div>;
+
+    return(
+      <div className={App}>
+        <p>Usuário: {user.login}</p>
+        <p>Biografia: {user.bio || 'Sem biografia'}</p>
+      </div>
+    );
+}
+
